b9f385b6ab47e1ce098ff753f6b32ab36c44b749
Kornberg <kornbergfresnel@outlook.com>
2017-05-31 11:21:37 +0800

    add usage information

--- Compiler/Compiler/main.cpp
+++ Compiler/Compiler/main.cpp
@@ -7,18 +7,32 @@
 //
 
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include "Lexer.hpp"
 #include "Parser.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::string test = "/Users/zhouming/GitHub/Compilers/Compiler/Tinytest/test.tiny";
-    std::string example = "/Users/zhouming/GitHub/Compilers/Compiler/Tinytest/example.tiny";
-    Lexer lexerMode(example);
-    lexerMode.lexer();
-    // lexerMode.show();
-    Parser parserModule(lexerMode.getTokenList());
-    parserModule.parser();
-    parserModule.show();
+	std::string info = "Usage: ./compiler -h [--help]\n\t-l [filename]\t--for running lexer\n\t-p [filename]\t--for running parser";
+	if (argc <= 2) {
+		std::cout << info << std::endl;
+	} else {
+		if (std::strcmp(argv[1], "-l") == 0) {
+			std::string file(argv[2]);
+			Lexer lexerMode(file);
+			lexerMode.lexer();
+			lexerMode.show();
+		} else if (std::strcmp(argv[1], "-p") == 0) {
+			std::string file(argv[2]);
+			Lexer lexerMode(file);
+			lexerMode.lexer();
+			Parser parseMoudle(lexerMode.getTokenList());
+			parseMoudle.parser();
+			parseMoudle.show();
+		} else if (std::strcmp(argv[1], "-h") == 0) {
+			std::cout << info << std::endl;
+		}
+	}
     return 0;
 }
